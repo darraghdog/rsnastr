d4345cefb3a6220caabb82eaff370d3929f2de2d
Darragh Hanley <darragh.hanley@gmail.com>
2020-09-13 17:57:51 +0100

    Make fols

---
+++ process/prepare_folds_v01.py
@@ -0,0 +1,24 @@
+import os
+import platform
+PATH = '/Users/dhanley/Documents/rsnastr' \
+        if platform.system() == 'Darwin' else '/data/rsnastr'
+os.chdir(PATH)
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import sys
+sys.path.insert(0, 'scripts')
+from utils.logs import get_logger
+logger = get_logger('Create folds', 'INFO') # noqa
+
+
+DATAPATH = 'data'
+trndf = pd.read_csv(os.path.join(DATAPATH, 'raw/train.csv.zip'))
+# tstdf = pd.read_csv(os.path.join(DATAPATH, 'raw/test.csv.zip'))
+
+logger.info('Create folds')
+folddf = pd.DataFrame({ 'StudyInstanceUID': trndf['StudyInstanceUID'].unique()})
+folddf['fold'] = (folddf.index.values)%5
+
+# Write out 
+folddf.to_csv(f'{DATAPATH}/folds.csz.gz', compression='gzip', index = False)
