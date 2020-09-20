95cd38eb23c7bf15e9f0c87fd083928676042905
Darragh Hanley <darragh.hanley@gmail.com>
2020-09-20 17:37:04 +0100

    Print balance

--- training/pipeline/train_image_classifier.py
+++ training/pipeline/train_image_classifier.py
@@ -198,7 +198,7 @@ for epoch in range(start_epoch, max_epochs):
     trnsampler = nSampler(trndataset.data, pe_weight = 0.66, nmin = 2, nmax = 4, seed = None)
     cts = trndataset.data.iloc[trnsampler.sample(trndataset.data)].pe_present_on_image.value_counts()
     logger.info(f'Epoch class balance:\n{cts}')
-    trnloader = DataLoader(trndataset, batch_size=args.batchsize*16, sampler = trnsampler, **loaderargs)
+    trnloader = DataLoader(trndataset, batch_size=args.batchsize, sampler = trnsampler, **loaderargs)
     model.train()
     pbar = tqdm(enumerate(trnloader), total=max_iters, desc="Epoch {}".format(current_epoch), ncols=0)
     if conf["optimizer"]["schedule"]["mode"] == "epoch":
