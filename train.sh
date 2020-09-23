2e5fe66ba89084de8b2c568d056fd5354612443b
Ubuntu <ubuntu@ip-172-31-26-94.eu-west-1.compute.internal>
2020-09-23 18:35:08 +0000

    Synch configs

--- train.sh
+++ train.sh
@@ -1,12 +1,29 @@
+python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/chkme' --augextra False  --label-smoothing 0.01 --config configs/_lr2308/rnxt101_lr1e4_binary.json
+python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/chkme' --augextra True  --label-smoothing 0.01 --config configs/_lr2308/rnxt101_lr1e4_binary.json
 
-python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 32 \
-    --logdir 'logs/nmin2_nmax4'  --label-smoothing 0.01 --config 'configs/nmin/rnxt101_binary_nmin2_nmax4.json'
-python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 32 \
-    --logdir 'logs/nmin2_nmax6'  --label-smoothing 0.01 --config 'configs/nmin/rnxt101_binary_nmin2_nmax6.json'
-python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 32 \
-    --logdir 'logs/nmin2_nmax8'  --label-smoothing 0.01 --config 'configs/nmin/rnxt101_binary_nmin2_nmax8.json'
-python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 32 \
-    --logdir 'logs/nmin2_nmax16'  --label-smoothing 0.01 --config 'configs/nmin/rnxt101_binary_nmin2_nmax16.json'
+#python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/chkme' --augextra False  --label-smoothing 0.01 --config configs/_lr2308/rnxt101_binary.json
+#python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/chkme' --augextra True  --label-smoothing 0.01 --config configs/_lr2308/rnxt101_binary.json
+
+#python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/chkme' --augextra True  --label-smoothing 0.01 --config configs/rnxt101_binary.json 
+#python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/chkme' --augextra False  --label-smoothing 0.01 --config configs/rnxt101_binary.json 
+
+
+#for RATIO in '05' '02' '005'
+#do 
+#    CONFIG=configs/peratio/rnxt101_binary_pe$RATIO.json
+#    LOGS=logs/rnxt101_peratio$RATIO
+#    python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 32 \
+#	    --logdir $LOGS  --label-smoothing 0.01 --config $CONFIG
+#done
+
+#python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 32 \
+#    --logdir 'logs/nmin2_nmax4'  --label-smoothing 0.01 --config 'configs/nmin/rnxt101_binary_nmin2_nmax4.json'
+#python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 32 \
+#    --logdir 'logs/nmin2_nmax6'  --label-smoothing 0.01 --config 'configs/nmin/rnxt101_binary_nmin2_nmax6.json'
+#python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 32 \
+#    --logdir 'logs/nmin2_nmax8'  --label-smoothing 0.01 --config 'configs/nmin/rnxt101_binary_nmin2_nmax8.json'
+#python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 32 \
+#    --logdir 'logs/nmin2_nmax16'  --label-smoothing 0.01 --config 'configs/nmin/rnxt101_binary_nmin2_nmax16.json'
 
 
 #python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 32 \
