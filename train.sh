f0fca52beb6a6bac5e6982a2531496b82ac2430c
Ubuntu <ubuntu@ip-172-31-26-94.eu-west-1.compute.internal>
2020-09-24 10:56:11 +0000

    Experiments

--- train.sh
+++ train.sh
@@ -1,5 +1,28 @@
-python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/chkme' --augextra False  --label-smoothing 0.01 --config configs/_lr2308/rnxt101_lr1e4_binary.json
-python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/chkme' --augextra True  --label-smoothing 0.01 --config configs/_lr2308/rnxt101_lr1e4_binary.json
+python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/focal' --augextra False  --label-smoothing 0.01 --config 'configs/_lr2308/rnxt101_lr1e4_binary_focal_pe0.25.json'
+# python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/chkme' --mixup_beta 0.3  --label-smoothing 0.01 --config configs/_lr2308/rnxt101_lr1e4_binary_mixup.json
+
+
+# Focal Loss
+## New best @
+#2020-09-24 10:09:01,506 - Train - INFO - Negimg PosStudy loss 0.2200 acc 0.9086; Posimg PosStudy loss 0.5284 acc 0.7400; Negimg NegStudy loss 0.1901 acc 0.9246; Avg 3 loss 0.3128 acc 0.8577
+#Epoch 18 improved from 0.31339 to 0.31283
+#Epoch: 18 bce: 0.31283, bce_best: 0.31283
+#python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/focal' --augextra False  --label-smoothing 0.01 --config configs/_lr2308/rnxt101_lr1e4_binary_focal.json
+
+
+## Best @ 
+#2020-09-23 18:37:20,842 - Train - INFO - Negimg PosStudy loss 0.2039 acc 0.9158; Posimg PosStudy loss 0.5814 acc 0.7278; Negimg NegStudy loss 0.1653 acc 0.9362; Avg 3 loss 0.3169 acc 0.8599
+#Epoch: 18 bce: 0.31687, bce_best: 0.31687
+
+#python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/chkme' --augextra False  --label-smoothing 0.01 --config configs/_lr2308/rnxt101_lr1e4_binary.json
+
+
+
+
+
+
+
+#python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/chkme' --augextra True  --label-smoothing 0.01 --config configs/_lr2308/rnxt101_lr1e4_binary.json
 
 #python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/chkme' --augextra False  --label-smoothing 0.01 --config configs/_lr2308/rnxt101_binary.json
 #python training/pipeline/train_image_classifier.py --device 'cuda' --fold 0 --batchsize 48 --logdir 'logs/chkme' --augextra True  --label-smoothing 0.01 --config configs/_lr2308/rnxt101_binary.json
