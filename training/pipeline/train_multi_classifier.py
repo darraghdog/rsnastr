460a68906c16137356ae49c073744ef71da22cb2
Darragh Hanley <darragh.hanley@gmail.com>
2020-10-09 19:26:00 +0100

    Multi classifier

--- training/pipeline/train_multi_classifier.py
+++ training/pipeline/train_multi_classifier.py
@@ -193,6 +193,7 @@ for epoch in range(args.epochs):
         xtrn = torch.autograd.Variable(xtrn, requires_grad=True)
         ytrn = torch.autograd.Variable(ytrn)
         ytrn = ytrn.view(-1, 10)
+        logger.info(xtrn.shape)
         with autocast():
             out = model(xtrn)
             out = out.view(-1, 10)
@@ -218,6 +219,7 @@ for epoch in range(args.epochs):
     for step, batch in pbarval:
         ytrn = batch['labels'].to(args.device, dtype=torch.float)
         xval = batch['image'].to(args.device, dtype=torch.float)
+        logger.info(xval.shape)
         yval = ytrn.view(-1, 10)
         out = model(xval)
         out = out.view(-1, 10)
