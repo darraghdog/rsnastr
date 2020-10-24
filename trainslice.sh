c18fa0ce7d3dfb0c35f77babf87a34ec351363d8
Darragh Hanley <darragh.hanley@gmail.com>
2020-10-24 13:03:13 +0100

    Change input channels

--- trainslice.sh
+++ trainslice.sh
@@ -1,4 +1,4 @@
-for FOLD in 0 # 1 2 3 # 0
+for FOLD in 0 3 4 # 1 2 3 # 0
 do
     python training/pipeline/train_image_slice_classifier.py --device 'cuda' --fold $FOLD --batchsize 32 --accum 4 --logdir 'logs/zoo' --flip True --augextra False --label-smoothing 0.005 --config 'configs/512/effnetb5_lr5e4_multi.json' 
 done
