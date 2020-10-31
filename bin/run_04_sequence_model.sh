7c08b879531e0a28fb22f1240567728c6a7d333f
darraghdog <darragh.hanley@doubleyard.com>
2020-11-01 03:12:45 +0900

    Finish up codebase

--- bin/run_04_sequence_model.sh
+++ bin/run_04_sequence_model.sh
@@ -1,7 +1,7 @@
 for FOLD in 0 1 2 3
 do
-    WEIGHTS='classifier__RSNAClassifier_tf_efficientnet_b5_ns_04d_fold'$FOLD'_img512_accum1___best__all_size512tmp.emb'
-    #python training/pipeline/train_sequence_classifier.py --device 'cuda' --fold $FOLD --imgemb $WEIGHTS --config configs/b5_seq_lstm.json
-    #python training/pipeline/train_sequence_classifier.py --device 'cuda' --fold $FOLD --imgemb $WEIGHTS --config configs/b5_seq_transformer_1layer.json
+    WEIGHTS='classifier__RSNAClassifier_tf_efficientnet_b5_ns_04d_fold'$FOLD'_img512_accum1___best__all_size512.emb'
+    python training/pipeline/train_sequence_classifier.py --device 'cuda' --fold $FOLD --imgemb $WEIGHTS --config configs/b5_seq_lstm.json
+    python training/pipeline/train_sequence_classifier.py --device 'cuda' --fold $FOLD --imgemb $WEIGHTS --config configs/b5_seq_transformer_1layer.json
     python training/pipeline/train_sequence_classifier.py --device 'cuda' --fold $FOLD --imgemb $WEIGHTS --config configs/b5_seq_transformer_2layer.json
 done
